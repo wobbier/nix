fc72852069adb5102d7ca31827b3d2a3a43f60b4
Mitch Andrews <rastaninja77@gmail.com>
2026-01-27 03:18:49 -0500

    🤒 Unstable + waybar update

--- flake.nix
+++ flake.nix
@@ -2,22 +2,53 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager?ref=release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    dolphin-overlay.url = "github:rumboon/dolphin-overlay";
+    warcraftlogs.url = "github:wobbier/warcraftlogs-nixos";
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs: {
+  outputs = { self, nixpkgs, dolphin-overlay, warcraftlogs, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
     nixosConfigurations.danktank = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
+      inherit system;
+
+      specialArgs = {
+        inherit inputs;
+      };
+
       modules = [
         ./configuration.nix
         ./hosts/danktank/configuration.nix
         ./hardware-configuration.nix
+
+        {
+          environment.systemPackages = [
+            warcraftlogs.packages.${system}.warcraftlogs
+          ];
+
+          nixpkgs.overlays = [
+            dolphin-overlay.overlays.default
+          ];
+        }
       ];
     };
+
     nixosConfigurations.virtualdank = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
+      inherit system;
+
+      specialArgs = {
+        inherit inputs;
+      };
+
       modules = [
         ./configuration.nix
         ./hosts/virtualdank/configuration.nix
