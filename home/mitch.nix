4c6a9b73902f3f385d3766c6f376daa74d1e5dee
Mitch Andrews <rastaninja77@gmail.com>
2026-05-29 22:26:02 -0400

    📦️ Adding Nixpkg search command

--- home/mitch.nix
+++ home/mitch.nix
@@ -18,6 +18,15 @@
   home.packages = with pkgs; [
     htop
     btop
+
+    ( pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        nix-search-tv
+      ];
+      text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+    } )
   ];
 
   programs.git = {
